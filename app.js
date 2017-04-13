2a761e838a9c2b00b76e6f2eaa800c2c031f030b
Ian Price <michael.ian.price@gmail.com>
2017-04-13 14:10:00 -0400

    Map page set up, db filled.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var yelp = new Yelp({
   app_secret: config.yelp.APPSECRET
 });
 var db = pgp(config.dbconfig);
+var current_result_set;
 
 var app = express();
 
@@ -32,33 +33,53 @@ app.post("/search_result", function(req, res, next){
   var transittype=req.body.transittype;
   var viewtype = req.body.viewtype;
   // Make a call to the yelp API
-  var place1 = "33.746314,-84.387667";
-  yelp.search({latitude: '33.746314', longitude: '-84.387667'})
+  yelp.search({location: '30062'})
     .then((data) => {
       data = JSON.parse(data);
-      data["businesses"].forEach((element) => {
-        var place2 = element.coordinates.latitude.toString() + "," + element.coordinates.longitude.toString();
-        var url = "https://maps.googleapis.com/maps/api/directions/json?origin="+place1+"&destination="+place2+"&mode=walking&key=AIzaSyBYC0MCm94ZwG-w5p9SR5PK25AYRjTEIw4"
-        request(url)
-        .then(function(str){
-          var obj = JSON.parse(str)
-          console.log(obj["routes"][0]["legs"][0]["duration"]["text"]);
-          console.log(element.name + " || " + element.coordinates.latitude + " || " + element.coordinates.longitude);
-          console.log();
-        })
+      // Filter the results on our end using marta db
 
-      });
+      // display the resultset
+      current_result_set = data;
+      if (viewtype === 'map') {
+        res.redirect('/map');
+      } else if (viewtype === 'table') {
+        res.redirect('/table');
+      }
+
+      // data["businesses"].forEach((element) => {
+      //   var place2 = element.coordinates.latitude.toString() + "," + element.coordinates.longitude.toString();
+      //   var url = "https://maps.googleapis.com/maps/api/directions/json?origin="+place1+"&destination="+place2+"&mode=walking&key=AIzaSyBYC0MCm94ZwG-w5p9SR5PK25AYRjTEIw4"
+      //   request(url)
+      //   .then(function(str){
+      //     var obj = JSON.parse(str)
+      //     console.log(obj["routes"][0]["legs"][0]["duration"]["text"]);
+      //     console.log(element.name + " || " + element.coordinates.latitude + " || " + element.coordinates.longitude);
+      //     console.log();
+      //   })
+      //
+      // });
     }).catch(next);
-  // Filter the results on our end using marta db
-
-  //display the resultset
-
 });
 
 app.get("/map", function(req, res, next){
+  var locations = current_result_set.businesses.map((element) => {
+    var html_string = `<img src="${element.image_url}"><br><strong>${element.name.replace("'","&#39;")}</strong><br><a href="${element.url}">Check out on Yelp</a><br><span>Rating: ${element.rating}</span><br><span>Price: ${element.price}`;
+    return {
+      info: html_string,
+      lat: element.coordinates.latitude,
+      long: element.coordinates.longitude
+    };
+  });
 
+  var coord = {
+    lat: current_result_set.region.center.latitude,
+    lon: current_result_set.region.center.longitude
+  };
 
-  res.render("map.hbs");
+  res.render("map.hbs", {
+    yelp_locations: locations,
+    coord: coord
+  });
 });
 
 app.get("/table", function(req, res, next){
@@ -67,11 +88,11 @@ app.get("/table", function(req, res, next){
   res.render("table.hbs");
 });
 
-app.get("/specific/:id", function(req, res, next){
-
-
-  res.render("specific.hbs");
-});
+// app.get("/specific/:id", function(req, res, next){
+//
+//
+//   res.render("specific.hbs");
+// });
 
 app.listen(9001, function(){
     console.log("listening on 9001");
